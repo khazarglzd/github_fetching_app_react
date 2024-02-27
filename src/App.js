be906de5820bf1ea04f8689dd65c82c05f68bf04
khazarglzd <khazarguluzade@gmail.com>
2024-02-27 20:48:05 +0400

    adding tailwind & Logo component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Logo from './components/Logo'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="min-h-screen bg-slate-900">
+      <div className='container text-gray-200 py-3'>
+        <Logo />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <div className="flex pb-2 justify-center items-center">
+            <img
+                src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-1024.png"
+                alt="logo"
+                className="w-24 bg-white rounded-full"
+            />
+            <h1 className="text-2xl px-2 "> Fetching Github Users App</h1>
+        </div>
+    );
+};
+
+export default Logo;
