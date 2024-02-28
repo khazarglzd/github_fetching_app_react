11c3dc5527a8ea6a4d84f4f9deb569533c33da32
khazarglzd <khazarguluzade@gmail.com>
2024-02-28 19:20:02 +0400

    adding Routing elements from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Logo from './components/Logo'
+import Users from './routes/Users'
+import { Routes, Route } from "react-router-dom"
 
 
 const App = () => {
   return (
     <div className="min-h-screen bg-slate-900">
-      <div className='container text-gray-200 py-3'>
+      <div className='container mx-auto text-gray-200 py-3'>
         <Logo />
+        <Routes>
+          <Route path='/' element={<Users />}></Route>
+        </Routes>
       </div>
     </div>
   )
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => {
     return (
-        <div className="flex pb-2 justify-center items-center">
+        <div className="flex pb-2 justify-center items-center border-b border-gray-500">
             <img
                 src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-social-github-1024.png"
                 alt="logo"
