4bf0557a2b69d2c5fc62e99b6491337f405a89cc
khazarglzd <khazarguluzade@gmail.com>
2024-03-01 21:29:41 +0400

    adding search functionality to Users component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-white">
-      <div className='container mx-auto text-gray-900 py-3'>
+    <div className="flex justify-center min-h-screen bg-white">
+      <div className="container text-gray-900 py-3">
         <Logo />
         <Routes>
           <Route path='/' element={<Users />}></Route>
--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,27 +1,82 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import UsersContainer from '../components/UsersContainer';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(null);
+    const user = useRef("");
+    let EndPoint = "https://api.github.com/users";
 
-    const [users, setUsers] = useState([])
-
-    let BaseUrl = "https://api.github.com/users";
-
-    async function AllUsers() {
-        const response = await fetch(BaseUrl);
-        const data = await response.json()
-        setUsers(data)
+    async function AllUesrs() {
+        if (user.current.value === "") {
+            setLoading(true);
+            const res = await fetch(EndPoint);
+            const data = await res.json();
+            setUsers(data);
+            setLoading(null);
+        }
     }
 
+    async function FindUser(e) {
+        setLoading(true);
+        if (user.current.value !== "") {
+            setUsers("");
+            const res = await fetch(EndPoint + "/" + user.current.value);
+            const data = await res.json();
+            setUsers(() => [data]);
+            console.log(users);
+            user.current.value = "";
+        } else {
+            AllUesrs();
+        }
+        setLoading(null);
+    }
+
+
     useEffect(() => {
-        AllUsers()
-    }, [setUsers])
+        AllUesrs();
+    }, [user, setUsers]);
+
 
     return (
         <div>
-            <UsersContainer users={users} />
-        </div>
-    )
-}
+            <div class="mb-3 flex justify-center">
+                <div class="flex w-[50%] flex-wrap items-center">
+                    <input
+                        type="text"
+                        class="relative m-0 -mr-0.5 block min-w-0 flex-auto rounded-l border outline-none border-solid border-green-500 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700"
+                        placeholder="Search a user..."
+                        aria-label="Search"
+                        aria-describedby="button-addon1"
+                        ref={user}
+                    />
 
-export default Users
+
+                    <button
+                        class="relative z-[2] flex items-center rounded-r bg-green-500 px-6 py-2 text-xs font-medium uppercase leading-tight text-white"
+                        type="button"
+                        id="button-addon1"
+                        data-te-ripple-init
+                        data-te-ripple-color="light"
+                        onClick={FindUser}
+                    >
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            class="h-5 w-5">
+                            <path
+                                fill-rule="evenodd"
+                                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div>{<UsersContainer users={users} />}</div>
+        </div>
+    );
+};
+
+export default Users;
