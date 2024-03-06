330fbaeeb09178c01527a875be9886af8b52a78e
khazarglzd <khazarguluzade@gmail.com>
2024-03-07 02:53:24 +0400

    creating Repo component

--- src/routes/UserInfo.js
+++ src/routes/UserInfo.js
@@ -26,52 +26,46 @@ const UserInfo = () => {
     }, [pathname]);
 
     return (
-        <div className="py-5">
-            <button
-                onClick={() => navigate("/")}
-                className="px-5 py-1 font-medium mx-1 my-4 bg-green-600 rounded text-white"
-            >
-                BACK
-            </button>
+        <div className="py-4">
             {user &&
                 user?.map((uinfo, i) => (
                     <div
                         key={i}
-                        className="flex justify-center md:flex-row
-               md:px-0 px-4 flex-col gap-10"
+                        className="flex md:flex-row
+               md:px-0 px-10 ml-20 flex-col gap-10"
                     >
                         <img
                             src={uinfo.avatar_url}
                             className="w-[350px] rounded-full border-4 border-green-300 md:mx-0 mx-auto"
                         />
-                        <div className="text-lg leading-10 px-3">
-                            <h1 className="text-3xl pb-4">{uinfo.name}</h1>
+                        <div className="text-lg leading-10 px-3 ml-6">
+                            <h1 className="text-3xl pb-4 pt-6">{uinfo.name}</h1>
                             <h1>
-                                <span className="text-green-500">Login Name</span> :{uinfo.login}
+                                <span className="text-green-500">Login Name: </span> :{uinfo.login}
                             </h1>
                             <h1>
-                                <span className="text-green-500">Followers : </span>
+                                <span className="text-green-500">Followers: </span>
                                 {uinfo.followers}
                             </h1>
                             <h1>
-                                <span className="text-green-500">Following : </span>
+                                <span className="text-green-500">Following: </span>
                                 {uinfo.following}
                             </h1>
                             <h1>
-                                <span className="text-green-500">Public Repositories : </span>
+                                <span className="text-green-500">Public Repositories: </span>
                                 {uinfo.public_repos}
                             </h1>
                             <h1>
-                                <span className="text-green-500">Join : </span>
+                                <span className="text-green-500">Join Date: </span>
                                 {new Date(uinfo.created_at).toLocaleDateString()}
                             </h1>
                             <a
                                 href={uinfo.html_url}
                                 target="_blank"
                                 className="text-white
-                    font-semibold rounded cursor-pointer  px-4 py-1 bg-green-600 my-3 tracking-wide"
+                    font-semibold rounded cursor-pointer  px-4 py-1 bg-green-500 my-3 tracking-wide"
                             >
-                                Visit
+                                Visit for More
                             </a>
                         </div>
                     </div>
